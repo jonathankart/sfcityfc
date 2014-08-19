a235be87607e288d443e1fcd02f2329b7bee1e03
Jonathan Kart <jonkart@gmail.com>
2014-08-19 13:51:44 -0700

    handled prod/dev different configs

--- conf/conf.php
+++ conf/conf.php
@@ -8,7 +8,14 @@ date_default_timezone_set('America/Los_Angeles');
 
 define('SFCITY_ROOT_PATH',realpath(__DIR__."/..")."/");
 
-$SFCITY_CONFIG->web_dir = SFCITY_ROOT_PATH."web/";
+if(file_exists(SFCITY_ROOT_PATH."public_html/")){
+	// this is the prod configuration
+	$SFCITY_CONFIG->web_dir = SFCITY_ROOT_PATH."public_html/";
+}else{
+	$SFCITY_CONFIG->web_dir = SFCITY_ROOT_PATH."web/";
+}
+
+
 $SFCITY_CONFIG->log_dir = SFCITY_ROOT_PATH."logs/";
 $SFCITY_CONFIG->cache_dir = SFCITY_ROOT_PATH."cache/";
 $SFCITY_CONFIG->documents_dir = $SFCITY_CONFIG->web_dir."docs/";
