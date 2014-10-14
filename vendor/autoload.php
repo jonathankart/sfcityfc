73ab4458eb1f143ea9abc7d24f1a5c91e4f9cf4e
Jonathan Kart <jonkart@gmail.com>
2014-10-14 12:07:33 -0700

    change tumblr version in composer

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit354f520db39f39767adf5d3c05c88d99::getLoader();
+return ComposerAutoloaderInit84f274fe23e000de91fffb133d8f10b0::getLoader();
